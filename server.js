9d13f3f147de02bf2abbea683fc9263ba4bc72d6
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-02-27 16:19:42 -0800

    change error response status code

--- server.js
+++ server.js
@@ -24,38 +24,8 @@ app.get('/books', async (request, response) => {
     })
     .catch(err => {
       console.log('error querying database');
-      response.send(err);
+      response.status(500).send(err);
     })
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-
-// var axios = require('axios');
-// var data = JSON.stringify({
-//     "collection": "books",
-//     "database": "books-db",
-//     "dataSource": "Cluster0",
-//     "projection": {
-//         "_id": 1
-//     }
-// });
-            
-// var config = {
-//     method: 'post',
-//     url: 'https://us-west-2.aws.data.mongodb-api.com/app/data-jwyeh/endpoint/data/v1/action/find',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Access-Control-Request-Headers': '*',
-//       'api-key': 'SrFraR6JIQ9NgJG8l0NyUcYsdwqcGpDAZiR8VUPn1wYzbie4dffpM9jsW20wVoM1',
-//     },
-//     data: data
-// };
-            
-// axios(config)
-//     .then(function (response) {
-//         console.log(JSON.stringify(response.data));
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     });
